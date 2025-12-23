9a874548d4183ba7385aa8d26968aed1113998f9
Chonlawit penwijit <43345050+chonlawit-pe61@users.noreply.github.com>
2025-12-23 23:34:30 +0700

    update

--- modules/Users/Models/CourseModel.php
+++ modules/Users/Models/CourseModel.php
@@ -22,13 +22,14 @@ class CourseModel extends Model
     {
         $builder = $this->db->table('member_course');
         $builder->select('member.*,member_course.*,tbl_prename.name as prename,tbl_course_level.name as level_name , course.name as course_name');
-        $builder->join('member', 'member.id = member_course.member_id', 'left');
+        $builder->join('member', 'member.id = member_course.member_id', 'INNER');
         $builder->join('tbl_prename', 'tbl_prename.id = member.prename_id', 'left');
         $builder->join('tbl_course_level', 'tbl_course_level.id = member_course.course_level_id', 'left');
         $builder->join('course', 'course.id = member_course.course_id', 'left');
         $builder->where('course_id', $id);
         $builder->orderBy('tbl_course_level.id', 'DESC');
         $data = $builder->get()->getResultArray();
+
         return $data;
     }
 }
